91fa9ac69d9f3820f271df4b71e66a6741c67dff
alexandercannon <a.m.t.cannon@gmail.com>
2016-02-21 19:27:44 +0000

    changes

--- js/scroll.js
+++ js/scroll.js
@@ -1,31 +1,15 @@
-//var tts = 0;
-//console.log(tts);
-//$(window).scroll(function(){
-//    if (tts < 10) {
-//        $(".bgBack").stop().animate({"marginTop": ($(window).scrollTop())/4 + "px"}, "slow" );
-//        $(".floor").stop().animate({"marginTop": ($(window).scrollTop())/20 + "px"}, "slow" );
-//        $(".forestBack").stop().animate({"marginTop": ($(window).scrollTop())/4 + "px"}, "slow" );
-//        $(".forestMid").stop().animate({"marginTop": ($(window).scrollTop())/6 + "px"}, "slow" );
-//        console.log($('floor').css('margin-top'));
-//        $(".forestFront").stop().animate({"marginTop": ($(window).scrollTop())/10 + "px"}, "slow" );
-//        $(".tower").stop().animate({"marginTop": ($(window).scrollTop())/6 + "px"}, "slow" );
-//        //  $(".main").stop().animate({"marginTop": ($(window).scrollTop()) + "px"}, "slow" );
-//        tts++;
-//  }
-//});
+
 $(document).on('ready', function() {  
     var winHeight = $(window).height(), 
         docHeight = $(document).height(),
         max, value, percent;
     max = docHeight - winHeight;
+    scrollController();
+    $('.popoverData').popover();
+    $('#popoverOption').popover({ trigger: "hover" });
 
     $(document).on('scroll', function(){
-        value = $(window).scrollTop();
-        console.log(value);
-        percent = Math.round(value/docHeight*100);
-        hideTitle(value, percent);
-        stickyRelocate(value);
-        pickTab(value);
+        scrollController();
     });
     $(window).on('resize', throttle(function() {
         winHeight = $(window).height(),
@@ -33,6 +17,17 @@ $(document).on('ready', function() {
         max = docHeight - winHeight;
         value =  $(window).scrollTop();
     }, 250));
+    function scrollController() {
+        value = $(window).scrollTop();
+        console.log(value);
+        percent = Math.round(value/docHeight*100);
+        hideTitle(value, percent);
+        stickyRelocate(value);
+        pickTab(value);
+        makeFullscreen(value);
+        animateForest(value);
+        hideClimber(value);
+    }
 });
 
 function throttle(fn, threshhold, scope) {
@@ -55,30 +50,37 @@ function throttle(fn, threshhold, scope) {
         }
     };
 }
-
+function hideClimber(height) {
+    if (height > 30) {
+        $('.climber-origin').removeClass('in');
+    } else {
+        $('.climber-origin').addClass('in');
+    }
+    if (height > 100) {
+        $('.holding').addClass('in');
+    } else { 
+        $('.holding').removeClass('in');
+    }
+}
 function hideTitle(height, percent) {
-//        console.log(percent);
     if (height > 2150) {
         $('.logo').removeClass('in');
-    }else if (height <2105) {
+    }else {
         $('.logo').addClass('in');
     }
-//    console.log(100-percent);
     $("#climber").stop().animate({"top": 80-percent + "%"}, "slow");
 }
 function stickyRelocate(height) {
     var window_top = $(window).scrollTop();
     var div_top = $('#social-buttons').offset().top;
-    if (height > 1260) {
+    if (height > 1400) {
         $('#social-buttons').addClass('stick');
-//        $('#social-buttons').width('45px');
     } else {
         $('#social-buttons').removeClass('stick');
-//        $('#social-buttons').width('100%');
     }
 }
 function pickTab(height) {
-    if (height > 590 && height < 650) {
+    if (height > 620 && height < 690) {
         $('#skills-toggle').addClass("active");
         $('#about-toggle').removeClass("active");
         $('#interests-toggle').removeClass("active");
@@ -88,19 +90,44 @@ function pickTab(height) {
         $('#about').removeClass("active");
         $('#interests').removeClass("in");
         $('#interests').removeClass("in");
-    } else if (height > 650) {
+    } else if (height > 690) {
         $('#interests-toggle').addClass("active");
         $('#skills-toggle').removeClass("active");
         $('#interests').addClass("in");
         $('#interests').addClass("active");
         $('#skills').removeClass("active");
         $('#skills').removeClass("in");
+        $('#about').removeClass("active");
+        $('#about').removeClass("active");
+        $('#about-toggle').removeClass("active");
     } else {
         $('#about-toggle').addClass("active");
         $('#skills-toggle').removeClass("active");
+        $('#interests-toggle').removeClass("active");
         $('#about').addClass("in");
         $('#about').addClass("active");
         $('#skills').removeClass("active");
         $('#skill').removeClass("in");
+        $('#interests').removeClass("in");
+        $('#interests').removeClass("in");
+    }
+}
+function makeFullscreen(height) {
+    if (height > 540 && height < 740) {
+        $('#mainBox').addClass('fullscreen');
+        $('#mainBox').addClass('fixed-box');
+    } else {
+         $('#mainBox').removeClass('fullscreen');
+         $('#mainBox').removeClass('fixed-box');
+    }
+}
+function animateForest(height) {
+    if (height < 520) {
+        $(".bgBack").stop().animate({"marginTop": ($(window).scrollTop())/2 + "px"}, "slow" );
+        $(".floor").stop().animate({"marginTop": ($(window).scrollTop())/10 + "px"}, "slow" );
+        $(".forestBack").stop().animate({"marginTop": ($(window).scrollTop())/4 + "px"}, "slow" );
+        $(".forestMid").stop().animate({"marginTop": ($(window).scrollTop())/6 + "px"}, "slow" );
+        $(".forestFront").stop().animate({"marginTop": ($(window).scrollTop())/10 + "px"}, "slow" );
+        $(".tower").stop().animate({"marginTop": ($(window).scrollTop())/6 + "px"}, "slow" );
     }
 }
